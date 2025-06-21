4f4a17c447f30516dbd40162b0172db1755d53fe
Laiba-muzammal <laibamuzammal7@gmail.com>
2025-06-21 20:59:07 +0500

    Commit all  changes

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from prompts.routes import prompts
 from models import db
+from prompts.routes import prompts
 
 app = Flask(__name__)
-app.config['SECRET_KEY']="The Millionaire Laiba"
 
+app.config['SECRET_KEY'] = 'The Millionaire Laiba'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///prompts.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -13,5 +13,4 @@ db.init_app(app)
 app.register_blueprint(prompts)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
-
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -6,3 +6,9 @@ class Prompts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     category = db.Column(db.String(100))
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(200), nullable=False)
+    email=db.Column(db.String(200), unique=True, nullable=False)
+    passsword=db.Column(db.String(200), nullable=False)
