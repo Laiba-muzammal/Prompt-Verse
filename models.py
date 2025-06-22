74bfaa3873a7aae080a9631a56033ab8f1667a11
Laiba-muzammal <laibamuzammal7@gmail.com>
2025-06-22 15:21:11 +0500

    Commit all changes

--- models.py
+++ models.py
@@ -12,3 +12,8 @@ class User(db.Model):
     name=db.Column(db.String(200), nullable=False)
     email=db.Column(db.String(200), unique=True, nullable=False)
     password=db.Column(db.String(200), nullable=False)
+
+
+def __repr__(self):
+    return f"<User {self.name} - {self.email}>"
+
