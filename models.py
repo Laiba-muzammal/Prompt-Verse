0b0866bfb2dc378a95d52d446c5580e9e78e1461
Laiba-muzammal <laibamuzammal7@gmail.com>
2025-06-22 16:11:24 +0500

    Commit all changes

--- models.py
+++ models.py
@@ -1,20 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-class Prompts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.Text, nullable=False)
-    category = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String(200), nullable=False)
-    email=db.Column(db.String(200), unique=True, nullable=False)
-    password=db.Column(db.String(200), nullable=False)
+    __tablename__ = 'users'  # recommended
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
 
+    prompts = db.relationship('Prompts', backref='user', lazy=True)
 
     def __repr__(self):
         return f"<User {self.name} - {self.email}>"
+
+class Prompts(db.Model):
+    __tablename__ = 'prompts'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+    
 
